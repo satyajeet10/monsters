5e5fe5798aaf76580f191f8bc58078827a9af1b9
Satyajeet Khanna <satyajeet.khanna@gmail.com>
2022-05-04 01:09:52 +0530

    :hammer: promises

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import { Component } from "react";
-
-import logo from "./logo.svg";
-import "./App.css";
-import { render } from "@testing-library/react";
-
+import { Component } from 'react';
+import './App.css';
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Linda",
-        },
-        {
-          name: "Frank",
-        },
-        {
-          name: "Jacky",
-        },
-        {
-          name: "Andrei",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => console.log(this.state)
+        )
+      );
   }
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         {this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
         })}
       </div>
     );
